f822dca60526a500cd05057ef8cfdad24f049730
Al-Zng <227518642+Al-Zng@users.noreply.github.com>
2026-05-13 16:52:54 -0400

    Initial commit: Add ZCinema application files (excluding workflows due to permissions)

---
+++ ZCinema/ZCinemaApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ZCinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
